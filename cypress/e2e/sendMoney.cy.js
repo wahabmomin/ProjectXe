9cd60925960cef57172be12bc81702926c809642
wahabahmed <wahabahmed102@gmail.com>
2022-11-18 11:28:25 +0000

    register user

--- cypress/e2e/sendMoney.cy.js
+++ cypress/e2e/sendMoney.cy.js
@@ -7,7 +7,16 @@
         //clicking Sign-in and send button
         cy.visit('https://www.xe.com/send-money/')
         cy.wait(3000)
+        //Register now button is disabled
         cy.get('.quoter__CostSpacedRow-bhpy0i-11 > .button__BaseButton-sc-1qpsalo-0').click()
+        cy.get('.ButtonBase-cwHurt').should('be.disabled')
+        //providing email and password
+        cy.get('#email').type('wahabahmed102@gmail.com')
+        cy.get('#password').type('Click123')
+        cy.wait(1000)
+        //Register now button is now enabled
+        cy.get('.ButtonBase-cwHurt').should('be.enabled')
+        cy.get('.ButtonBase-cwHurt').click()
         
       })
 
